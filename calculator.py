082fb7376b647d1bfa8db316fd45e15cacee280b
AAjayiB <72974478+AAjayiB@users.noreply.github.com>
2022-10-08 22:01:43 +0100

    calculator implemented

---
+++ calculator.py
@@ -0,0 +1,30 @@
+import re
+
+def calculate(input):
+    validExpression = re.findall("\d+|[\+\-\*]", number)
+    # validOperator = re.findall("[\+\-\*]", number)
+    # print(f"The numbers: {validExpression}\nThe operators: {validOperator}\nThird integer is: {int(validExpression[2])+int(validExpression[4])}")
+    
+    while (validExpression.count("*") > 0):
+        opIndex = validExpression.index("*")
+        computedValue = int(validExpression[opIndex-1])*int(validExpression[opIndex+1])
+        validExpression[opIndex-1] = computedValue
+        validExpression.pop(opIndex+1)
+        validExpression.pop(opIndex)
+    
+    while (validExpression.count("+") > 0):
+        opIndex = validExpression.index("+")
+        computedValue = int(validExpression[opIndex-1])+int(validExpression[opIndex+1])
+        validExpression[opIndex-1] = computedValue
+        validExpression.pop(opIndex+1)
+        validExpression.pop(opIndex)
+        
+    while (validExpression.count("-") > 0):
+        opIndex = validExpression.index("-")
+        computedValue = int(validExpression[opIndex-1])-int(validExpression[opIndex+1])
+        validExpression[opIndex-1] = computedValue
+        validExpression.pop(opIndex+1)
+        validExpression.pop(opIndex)
+    return str(validExpression[0])
+
+    
