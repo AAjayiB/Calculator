329fe3e9d54955c6ea414d6b47df54aa4df7c52f
AAjayiB <72974478+AAjayiB@users.noreply.github.com>
2022-10-11 12:38:10 +0100

    wrote the code from scratch

--- calculator.py
+++ calculator.py
@@ -1,42 +1,45 @@
-import re
-
 def calculate(exp):
-    answer = ""
     if exp:
-        if re.search("[^0-9]+|[^\+\-\*]", exp):
-            answer = "Not a valid input"
+        # checks for any non operators or digits
+        if not re.search("[^\d\+\-\*]",exp):
+            # checks for expressions beginning only with a single minus or not
+            if re.search("^\-?\d",exp):
+                # checks for expressions ending with only digits 
+                if re.search("\d$", exp):
+                    ############# ACTUAL OPERATIONS #############
+                    validExpression = re.findall("\d+|[\+\-\*]", exp)
+                    ############# Here at the moment
+                    if validExpression[0] =="-":
+                        validExpression[0:2]=["".join(validExpression[0:2])]
+
+                    while (validExpression.count("*") > 0):
+                        opIndex = validExpression.index("*")
+                        computedValue = int(validExpression[opIndex-1])*int(validExpression[opIndex+1])
+                        validExpression[opIndex-1] = computedValue
+                        validExpression.pop(opIndex+1)
+                        validExpression.pop(opIndex)
+
+                    while (validExpression.count("-") > 0):
+                        opIndex = validExpression.index("-")
+                        computedValue = int(validExpression[opIndex-1])-int(validExpression[opIndex+1])
+                        validExpression[opIndex-1] = computedValue
+                        validExpression.pop(opIndex+1)
+                        validExpression.pop(opIndex)
+                    
+                    while (validExpression.count("+") > 0):
+                        opIndex = validExpression.index("+")
+                        computedValue = int(validExpression[opIndex-1])+int(validExpression[opIndex+1])
+                        validExpression[opIndex-1] = computedValue
+                        validExpression.pop(opIndex+1)
+                        validExpression.pop(opIndex)
+
+                    print(str(validExpression[0]))
+
+                else:
+                    print("Expression cannot end with an operation")
+            else:
+                print("Expression cannot begin with an operation")
         else:
-            validExpression = re.findall("\d+|[\+\-\*]", exp)
-            # validOperator = re.findall("[\+\-\*]", number)
-            # print(f"The numbers: {validExpression}\nThe operators: {validOperator}\nThird integer is: {int(validExpression[2])+int(validExpression[4])}")
-            
-            while (validExpression.count("*") > 0):
-                opIndex = validExpression.index("*")
-                computedValue = int(validExpression[opIndex-1])*int(validExpression[opIndex+1])
-                validExpression[opIndex-1] = computedValue
-                validExpression.pop(opIndex+1)
-                validExpression.pop(opIndex)
-
-            while (validExpression.count("-") > 0):
-                opIndex = validExpression.index("-")
-                computedValue = int(validExpression[opIndex-1])-int(validExpression[opIndex+1])
-                validExpression[opIndex-1] = computedValue
-                validExpression.pop(opIndex+1)
-                validExpression.pop(opIndex)
-            
-            while (validExpression.count("+") > 0):
-                opIndex = validExpression.index("+")
-                computedValue = int(validExpression[opIndex-1])+int(validExpression[opIndex+1])
-                validExpression[opIndex-1] = computedValue
-                validExpression.pop(opIndex+1)
-                validExpression.pop(opIndex)
-                
-            
-            return str(validExpression[0])
+            print("Not a valid input")
     else:
-        answer = "No input was entered."
-
-    return answer
-
-
-    
+        print("No input was entered")
