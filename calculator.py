d6e46921d8f849a2177b081b90b5c56b1accf4c7
AAjayiB <72974478+AAjayiB@users.noreply.github.com>
2022-10-11 15:37:05 +0100

    edited subtraction conditions

--- calculator.py
+++ calculator.py
@@ -34,7 +34,8 @@ def calculate(exp):
 
                     while (validExpression.count("-") > 0):
                         opIndex = validExpression.index("-")
-                        
+                        if validExpression[opIndex+1]=="-":
+                            return CONSECUTIVE_OPERATORS
                         if validExpression[opIndex-1]=="+":
                             validExpression.pop(opIndex-1)
                         else:
